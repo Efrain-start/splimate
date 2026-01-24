9f7588ba53e07e1d8a40c11f6cee7c81d1521f3a
Efrain Aguilar <efrainaguilar1978@gmail.com>
2026-01-23 23:27:33 -0600

    Fix build: remove auth reducer

--- src/app/store.js
+++ src/app/store.js
@@ -2,13 +2,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import groupsReducer from "../features/groups/groupsSlice";
 import { loadState, saveState } from "./localStorage";
-import authReducer from "../features/auth/authSlice";
 
 const preloadedState = loadState();
 
 export const store = configureStore({
   reducer: {
-    auth: authReducer,
     groups: groupsReducer,
   },
   preloadedState,
